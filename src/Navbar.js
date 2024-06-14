9ddeec8d830e4bcf8cce2ba0759a20ad80ac421d
Alec Scott <asjscott@gmail.com>
2024-06-14 14:11:38 -0700

    updated navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -44,38 +44,97 @@ const Navbar = () => {
             data-twe-collapse-item>
 
             <a
-                className="mb-4 me-5 ms-2 mt-3 flex items-center text-neutral-900 hover:text-neutral-900 focus:text-neutral-900 dark:text-neutral-200 dark:hover:text-neutral-400 dark:focus:text-neutral-400 lg:mb-0 lg:mt-0"
-                href="https://asjscott.github.io">
+                class="mb-4 me-5 ms-2 mt-3 flex items-center text-black/50 transition duration-200 hover:text-neutral-700 hover:ease-in-out focus:text-neutral-900 dark:text-white/60 dark:hover:text-white/80 dark:focus:text-white/80 dark:active:text-white/80 lg:mb-0 lg:mt-1 lg:px-2"
+                href="https://asjscott.github.io/#">
+                Alec Scott  
                 <img
                 src={logo}
                 style={{height: "25px"}}
                 alt="Logo"
                 loading="lazy" />
-            </a>
+             </a>
 
             <ul
                 className="list-style-none me-auto flex flex-col ps-0 lg:flex-row"
                 data-twe-navbar-nav-ref>
-                <li className="mb-4 lg:mb-0 lg:pe-2" data-twe-nav-item-ref>
+                
+                <li
+                class="mb-4 ps-2 lg:mb-0 lg:pe-1 lg:ps-0"
+                data-twe-nav-item-ref
+                data-twe-dropdown-ref>
+
+                <a
+                    class="flex items-center text-black/50 transition duration-200 hover:text-black/80 hover:ease-in-out focus:text-black/80 active:text-black/80 motion-reduce:transition-none dark:text-white/60 dark:hover:text-white/80 dark:focus:text-white/80 dark:active:text-white/80 lg:px-2"
+                    href="#"
+                    type="button"
+                    id="dropdownMenuButton2"
+                    data-twe-dropdown-toggle-ref
+                    aria-expanded="false">
+                    Projects
+                    <span class="ms-1 [&>svg]:w-5">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 20 20"
+                        fill="currentColor">
+                        <path
+                        fill-rule="evenodd"
+                        d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
+                        clip-rule="evenodd" />
+                    </svg>
+                    </span>
+                </a>
+                <ul
+                    class="absolute z-[1000] float-left m-0 hidden min-w-max list-none overflow-hidden rounded-lg border-none bg-white bg-clip-padding text-left text-base shadow-lg data-[twe-dropdown-show]:block dark:bg-surface-dark"
+                    aria-labelledby="dropdownMenuButton1"
+                    data-twe-dropdown-menu-ref>
+                    <li>
+                    <a
+                        class="block w-full whitespace-nowrap bg-white px-4 py-2 text-sm font-normal text-neutral-700 hover:bg-zinc-200/60 focus:bg-zinc-200/60 focus:outline-none active:bg-zinc-200/60 active:no-underline dark:bg-surface-dark dark:text-white dark:hover:bg-neutral-800/25 dark:focus:bg-neutral-800/25 dark:active:bg-neutral-800/25"
+                        href="https://asjscott.github.io/#projects"
+                        data-twe-dropdown-item-ref
+                        >View projects section</a>
+                    </li>
+                    <li>
+                    <a
+                    class="block w-full whitespace-nowrap bg-white px-4 py-2 text-sm font-normal pointer-events-none text-black/30 dark:bg-surface-dark dark:text-white/30"
+                    >Football score predictor</a>
+                </li>
+                    <li>
+                    <a
+                        class="block w-full whitespace-nowrap bg-white px-4 py-2 text-sm font-normal text-neutral-700 hover:bg-zinc-200/60 focus:bg-zinc-200/60 focus:outline-none active:bg-zinc-200/60 active:no-underline dark:bg-surface-dark dark:text-white dark:hover:bg-neutral-800/25 dark:focus:bg-neutral-800/25 dark:active:bg-neutral-800/25"
+                        href="https://asjscott.github.io/pomodoro"
+                        data-twe-dropdown-item-ref
+                        >React projects</a>
+                    </li>
+                    <li>
+                    <a
+                        class="block w-full whitespace-nowrap bg-white px-4 py-2 text-sm font-normal pointer-events-none text-black/30 dark:bg-surface-dark dark:text-white/30"
+                        >Trading Bot</a
+                    >
+                    </li>
+                </ul>
+                </li>
+
+                <li className="mb-4 ps-2 lg:mb-0 lg:pe-1 lg:ps-0" data-twe-nav-item-ref>
 
                 <a  id="projectslink"
-                    className="text-neutral-500 transition duration-200 hover:text-neutral-700 hover:ease-in-out focus:text-neutral-700 disabled:text-black/30 motion-reduce:transition-none dark:text-neutral-200 dark:hover:text-neutral-300 dark:focus:text-neutral-300 lg:px-2 [&.active]:text-black/90 dark:[&.active]:text-zinc-400"
+                    className="flex items-center text-black/50 transition duration-200 hover:text-black/80 hover:ease-in-out focus:text-black/80 active:text-black/80 motion-reduce:transition-none dark:text-white/60 dark:hover:text-white/80 dark:focus:text-white/80 dark:active:text-white/80 lg:px-2"
                     href="https://asjscott.github.io/pomodoro"
                     data-twe-nav-link-ref
                     >Pomodoro Timer</a>
                 </li>
 
-                <li className="mb-4 lg:mb-0 lg:pe-2" data-twe-nav-item-ref>
+                <li className="mb-4 ps-2 lg:mb-0 lg:pe-1 lg:ps-0" data-twe-nav-item-ref>
                 <a
-                    className="text-neutral-500 transition duration-200 hover:text-neutral-700 hover:ease-in-out focus:text-neutral-700 disabled:text-black/30 motion-reduce:transition-none dark:text-neutral-200 dark:hover:text-neutral-300 dark:focus:text-neutral-300 lg:px-2 [&.active]:text-black/90 dark:[&.active]:text-neutral-400"
+                    className="flex items-center text-black/50 transition duration-200 hover:text-black/80 hover:ease-in-out focus:text-black/80 active:text-black/80 motion-reduce:transition-none dark:text-white/60 dark:hover:text-white/80 dark:focus:text-white/80 dark:active:text-white/80 lg:px-2"
                     href="https://asjscott.github.io/calculator"
                     data-twe-nav-link-ref
                     >Calculator</a >
                 </li>
 
-                <li className="mb-4 lg:mb-0 lg:pe-2" data-twe-nav-item-ref>
+                <li className="mb-4 ps-2 lg:mb-0 lg:pe-1 lg:ps-0" data-twe-nav-item-ref>
                 <a
-                    className="text-neutral-500 transition duration-200 hover:text-neutral-700 hover:ease-in-out focus:text-neutral-700 disabled:text-black/30 motion-reduce:transition-none dark:text-neutral-200 dark:hover:text-neutral-300 dark:focus:text-neutral-300 lg:px-2 [&.active]:text-black/90 dark:[&.active]:text-neutral-400"
+                    className="flex items-center text-black/50 transition duration-200 hover:text-black/80 hover:ease-in-out focus:text-black/80 active:text-black/80 motion-reduce:transition-none dark:text-white/60 dark:hover:text-white/80 dark:focus:text-white/80 dark:active:text-white/80 lg:px-2"
                     href="https://asjscott.github.io/drum_machine"
                     data-twe-nav-link-ref
                     >Drum Machine</a >
@@ -84,12 +143,12 @@ const Navbar = () => {
             </div>
 
 
-            <div id="navbar-icons" className="mobile-only:py-2 relative flex items-center">
+            <div id="navbar-icons" className="mobile-only:py-2 ps-2 lg:mb-0 lg:pe-1 lg:ps-0 relative flex items-center">
 
                 <a
                 href="https://www.linkedin.com/in/alec-scott-499670280/"
                 type="button"
-                className="mobile-only:text-neutral-500 me-4 text-white transition duration-200 hover:text-neutral-700 hover:ease-in-out focus:text-neutral-700 disabled:text-black/30 motion-reduce:transition-none dark:text-neutral-200 dark:hover:text-neutral-300 dark:focus:text-neutral-300 [&.active]:text-black/90 dark:[&.active]:text-neutral-400"
+                className="mobile-only:text-neutral-500 me-4 text-black transition duration-200 hover:text-neutral-700 hover:ease-in-out focus:text-neutral-700 disabled:text-black/30 motion-reduce:transition-none dark:text-neutral-200 dark:hover:text-neutral-300 dark:focus:text-neutral-300 [&.active]:text-black/90 dark:[&.active]:text-neutral-400"
                data-twe-ripple-init>
                 <span className="mx-auto [&>svg]:h-5 [&>svg]:w-5">
                     <svg
@@ -105,7 +164,7 @@ const Navbar = () => {
                 <a
                 href="https://www.github.com/asjscott"
                 type="button"
-                className="mobile-only:text-neutral-500 me-4 text-white transition duration-200 hover:text-neutral-700 hover:ease-in-out focus:text-neutral-700 disabled:text-black/30 motion-reduce:transition-none dark:text-neutral-200 dark:hover:text-neutral-300 dark:focus:text-neutral-300 [&.active]:text-black/90 dark:[&.active]:text-neutral-400"
+                className="mobile-only:text-neutral-500 me-4 text-black transition duration-200 hover:text-neutral-700 hover:ease-in-out focus:text-neutral-700 disabled:text-black/30 motion-reduce:transition-none dark:text-neutral-200 dark:hover:text-neutral-300 dark:focus:text-neutral-300 [&.active]:text-black/90 dark:[&.active]:text-neutral-400"
                 data-twe-ripple-init>
                 <span className="mx-auto [&>svg]:h-5 [&>svg]:w-5">
                     <svg
